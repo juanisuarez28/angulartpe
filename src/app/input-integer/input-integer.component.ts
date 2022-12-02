29a7ba463a1d206b562406f266467013830f6bf6
juanisuarez28 <juanisua3@gmail.com>
2022-12-02 13:28:53 -0300

    input-output hecho

--- src/app/input-integer/input-integer.component.ts
+++ src/app/input-integer/input-integer.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Product } from '../product-list/Product';
 
 @Component({
@@ -10,22 +10,34 @@ export class InputIntegerComponent implements OnInit {
 
   constructor() { }
 
-  @Input()
-  product!: Product;
+  @Input() 
+  quantity!: number;
+
+  @Input() 
+  max!:number;
+
+  @Output() 
+  quantityChange: EventEmitter<number>=new EventEmitter<number>();
 
   ngOnInit(): void {
   }
-  upQuantity(product: Product): void{
-    if(product.quantity < product.stock)
-      product.quantity++;
-  }
-  downQuantity(product: Product): void{
-    if(product.quantity>0)  
-      product.quantity--;
+
+  upQuantity(): void{
+    if(this.quantity < this.max) {
+      this.quantity++;
+      this.quantityChange.emit(this.quantity);
+    }
   }
 
-  changeQuantity(event: any, product: Product): void{
-    if(event.key>0 && event.key<=9)
-      event.preventDefault();
+  downQuantity(): void{
+    if(this.quantity>0) {
+      this.quantity--;
+      this.quantityChange.emit(this.quantity);
+    }  
+  }
+
+  changeQuantity(event: any): void{
+    console.log(event.key);
+    this.quantityChange.emit(this.quantity);
   }
 }
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -43,5 +43,9 @@ export class ProductListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  maxReached(m:String){
+    alert(m);
+  }
+
 
 }
